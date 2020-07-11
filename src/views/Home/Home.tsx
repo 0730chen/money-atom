ffa2b9ce85e45e0f9632c2c8ec81ae2b79f9af47
0730chen <1252331215@qq.com>
2020-07-11 19:52:55 +0800

    feat:数字键盘样式

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -71,7 +71,56 @@ const TypeSection = styled.section`
   }
 `
 const NumberPad = styled.section`
-  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  >.out-put{
+  background: white;
+   font-size: 36px;
+    line-height: 72px;
+   text-align: right;
+   padding: 0 16px;
+   box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+               inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  >.number-pad{
+    background: white;
+    >button{
+    font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      text-align: center;
+      border: none;
+        &.ok{
+           height: 128px;
+          float: right;
+        }
+        &.dot{
+            width: 50%;
+        }
+       &:nth-child(1){
+        background: rgb(234,214,241)
+     }
+    &:nth-child(2),&:nth-child(5){
+        background: rgb(223,220,241)
+    }
+    &:nth-child(3),&:nth-child(6),&:nth-child(9){
+      background: rgb(214,219,238)
+    }
+    &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
+      background: rgba(205,223,238);
+    }
+    &:nth-child(8),&:nth-child(11){
+      background: rgba(196,229,238);
+    }
+    &:nth-child(12){
+    background: rgba(187,230,236);
+    }
+    &:nth-child(14){
+      background: #a0f1ea;
+    }
+  }
+  }
 `
 
 const Home = ()=>(
@@ -97,17 +146,25 @@ const Home = ()=>(
                     <button>支出</button>
                 </TypeSection>
                 <NumberPad>
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                    </ul>
+                    <div className="out-put">
+                        100
+                    </div>
+                    <div className="number-pad clear-fix">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>删除</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>清空</button>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button className="ok">ok</button>
+                        <button>0</button>
+                        <button className="dot">.</button>
+                    </div>
                 </NumberPad>
             </Container>
             <NavBar/>
